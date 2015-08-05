753680be211182fd3e8d1fad592db143ff43f7fe
Sebastian Ellefsen <sebellefsen@gmail.com>
2015-08-05 19:16:25 +0200

    Folders

---
+++ game_over/game_over/game_over/Source.cpp
@@ -0,0 +1,12 @@
+//This is a game_over program
+//It prints text to the screen
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(){
+	cout << "Game Over!" << endl;
+	return 0;
+}
